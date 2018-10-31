c56b1a833078999728a85c5296bae246d36a6a15
Javier Cruz Muñoz <jcruz@invidgroup.com>
2018-10-31 18:13:05 -0400

    first commit

---
+++ src/chat-server.ts
@@ -0,0 +1,69 @@
+import { createServer, Server } from 'http';
+import * as express from 'express';
+import * as socketIo from 'socket.io';
+
+import { Message } from './models/message.model';
+
+export class ChatServer {
+
+    public static readonly PORT:number = 8080;
+    
+    private app: express.Application;
+    //represent the entity
+    private server: Server;
+    //comunication
+    private io: SocketIO.Server;
+    //the port
+    private port: string | number;
+
+    constructor() {
+        this.createApp();
+        this.config();
+        this.createServer();
+        this.sockets();
+        this.listen();
+    }
+
+    private createApp(): void {
+        this.app = express();
+    }
+
+    private createServer(): void {
+        this.server = createServer(this.app);
+    }
+
+    private config(): void {
+        this.port = process.env.PORT || ChatServer.PORT;
+    }
+
+    private sockets(): void {
+        this.io = socketIo(this.server);
+    }
+
+    //listen activities in the server
+    private listen(): void {
+
+        this.server.listen(this.port, () => {
+            console.log('Running server on port %s', this.port);
+        });
+
+        this.io.on('connect', (socket: any) => {
+            console.log('Connected client on port %s.', this.port);
+    
+            //message management
+            socket.on('message', (m: Message) => {
+                console.log('[server](message): %s', JSON.stringify(m));
+                this.io.emit('message', m);
+            });
+
+            socket.on('disconnect', () => {
+                console.log('Client disconnected');
+            });
+        });
+    }
+
+    //get the entiti
+    public getApp(): express.Application {
+        return this.app;
+    }
+}
---
+++ src/models/message.model.ts
@@ -0,0 +1,6 @@
+import { User } from "./user.model";
+
+export class Message{
+    from:User;
+    content:string;
+}
