cef96004134559d761d5a333d8a2fe1c5ce2cc5a
Javier Cruz Muñoz <jcruz@invidgroup.com>
2018-11-01 17:46:30 -0400

    chat structured

--- dist/chat-server.js
+++ dist/chat-server.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var http_1 = require("http");
 var express = require("express");
 var socketIo = require("socket.io");
+/*
+Connect server main class
+*/
 var ChatServer = /** @class */ (function () {
     function ChatServer() {
         this.createApp();
@@ -11,30 +14,42 @@ var ChatServer = /** @class */ (function () {
         this.sockets();
         this.listen();
     }
+    //setter instance
     ChatServer.prototype.createApp = function () {
         this.app = express();
     };
     ChatServer.prototype.createServer = function () {
         this.server = http_1.createServer(this.app);
     };
+    //setter of port to use
     ChatServer.prototype.config = function () {
         this.port = process.env.PORT || ChatServer.PORT;
     };
+    //setter comunication
     ChatServer.prototype.sockets = function () {
         this.io = socketIo(this.server);
     };
-    //listen activities in the server
+    //listen activities in the server events
     ChatServer.prototype.listen = function () {
         var _this = this;
+        //when the server is ON and listening
         this.server.listen(this.port, function () {
             console.log('Running server on port %s', _this.port);
         });
+        //client connect
         this.io.on('connect', function (socket) {
             console.log('Connected client on port %s.', _this.port);
-            //message management
+            //create a room TODO
+            //message send management
             socket.on('message', function (m) {
+                //put message object into json object and send it
                 console.log('[server](message): %s', JSON.stringify(m));
                 _this.io.emit('message', m);
+            });
+            //notify the tech client is waiting
+            socket.on('notify', function (data) {
+                console.log('(notify):%s', JSON.stringify(data));
+                _this.io.emit('notify', data);
             });
             socket.on('disconnect', function () {
                 console.log('Client disconnected');
@@ -45,6 +60,7 @@ var ChatServer = /** @class */ (function () {
     ChatServer.prototype.getApp = function () {
         return this.app;
     };
+    //port that the chat will handle
     ChatServer.PORT = 8080;
     return ChatServer;
 }());
--- src/chat-server.ts
+++ src/chat-server.ts
@@ -4,16 +4,24 @@ import * as socketIo from 'socket.io';
 
 import { Message } from './models/message.model';
 
+/*
+Connect server main class
+*/
 export class ChatServer {
 
+    //port that the chat will handle
     public static readonly PORT:number = 8080;
     
+    //represent the whole app
     private app: express.Application;
+   
     //represent the entity
     private server: Server;
-    //comunication
+  
+    //comunication that will send events
     private io: SocketIO.Server;
-    //the port
+
+    //the port to use
     private port: string | number;
 
     constructor() {
@@ -24,6 +32,7 @@ export class ChatServer {
         this.listen();
     }
 
+    //setter instance
     private createApp(): void {
         this.app = express();
     }
@@ -32,28 +41,43 @@ export class ChatServer {
         this.server = createServer(this.app);
     }
 
+    //setter of port to use
     private config(): void {
         this.port = process.env.PORT || ChatServer.PORT;
     }
-
+    
+    //setter comunication
     private sockets(): void {
         this.io = socketIo(this.server);
     }
 
-    //listen activities in the server
+    //listen activities in the server events
     private listen(): void {
 
+        //when the server is ON and listening
         this.server.listen(this.port, () => {
             console.log('Running server on port %s', this.port);
         });
 
+        //client connect
         this.io.on('connect', (socket: any) => {
             console.log('Connected client on port %s.', this.port);
-    
-            //message management
+            
+            //create a room TODO
+            
+
+            //message send management
             socket.on('message', (m: Message) => {
+                //put message object into json object and send it
                 console.log('[server](message): %s', JSON.stringify(m));
                 this.io.emit('message', m);
+            });
+
+
+            //notify the tech client is waiting
+            socket.on('notify', (data:any)=>{
+                console.log('(notify):%s', JSON.stringify(data));
+                this.io.emit('notify', data);
             });
 
             socket.on('disconnect', () => {
